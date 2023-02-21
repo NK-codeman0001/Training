59ee3b7bc93b99beda8e31083e4a5dd4d8e2d391
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-21 14:32:01 +0530

    merged db branch

---
+++ ruby/test.rb
@@ -0,0 +1,5 @@
+puts "Heelllo"
+p "hellpo"
+print "hello print1"
+print "hello priint2"
+
---
+++ ruby/test1.rb
@@ -0,0 +1,8 @@
+# arr1 = Array(1..5);
+# arr2 = Array(1..5);
+# arr3 = arr1.collect * arr2.collect;
+# puts arr3;
+val=gets;
+puts val; 
+# str="Jai Mata Di\n\n"
+# puts str
