1164958f4416a4ffb70508d839d7f38dee463033
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 14:51:24 +0530

    add charging_modes table values

---
+++ database/cab_booking_db/charging_modes.sql
@@ -0,0 +1,3 @@
+insert into charging_modes (id, mode) values (1, 'per hour');
+insert into charging_modes (id, mode) values (2, 'per kilometer');
+insert into charging_modes (id, mode) values (3, 'per day');
