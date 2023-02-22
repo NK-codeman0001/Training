91dbdd9e420ddb34d45cd53c3c491f9ae1a9f343
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 14:49:39 +0530

    add cab_types table values

---
+++ database/cab_booking_db/cab_types.sql
@@ -0,0 +1,5 @@
+insert into cab_types (id, type, base_charge, charge_type, per_charge) values (1, 'auto', 10, 2, 8);
+insert into cab_types (id, type, base_charge, charge_type, per_charge) values (2, 'bike', 20, 2, 10);
+insert into cab_types (id, type, base_charge, charge_type, per_charge) values (3, 'regular', 100, 2, 20);
+insert into cab_types (id, type, base_charge, charge_type, per_charge) values (4, 'prime', 500, 1, 50);
+insert into cab_types (id, type, base_charge, charge_type, per_charge) values (5, 'suv',400,2, 50);
