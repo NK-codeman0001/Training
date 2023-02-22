cb8d87e3231215c93b165f1bfe2cc1b8f06db52d
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 14:52:43 +0530

    add other_charges table values

---
+++ database/cab_booking_db/other_charges.sql
@@ -0,0 +1,24 @@
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (1, 1,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (1, 2,50);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (1, 3,50);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (1, 4,30);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (2, 1,0);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (2, 2,50);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (2, 3,50);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (2, 4,30);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (3, 1,150);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (3, 2,60);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (3, 3,60);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (3, 4,50);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (4, 1,200);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (4, 2,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (4, 3,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (4, 4,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (5, 1,200);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (5, 2,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (5, 3,100);
+insert into other_charges (cab_types_id, miscellaneous_charges_id, charge) values (5, 4,100);
+
+
+
+
