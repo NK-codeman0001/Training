1a14dc34b520dcd7c0380c9805aed92e22b43573
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 17:30:26 +0530

    add countries table values

---
+++ database/cab_booking_db/countries.sql
@@ -0,0 +1,5 @@
+insert into countries (id, name) values (1, 'India');
+insert into countries (id, name) values (2, 'Pakistan');
+insert into countries (id, name) values (3, 'Bangladesh');
+insert into countries (id, name) values (4, 'Nepal');
+insert into countries (id, name) values (5, 'Sri Lanka');
