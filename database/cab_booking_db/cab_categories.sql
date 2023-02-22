083f1413a5e6614e6f679cdd5670411c24e70554
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 14:49:07 +0530

    add cab_categories table values

---
+++ database/cab_booking_db/cab_categories.sql
@@ -0,0 +1,5 @@
+insert into cab_categories (id, category) values (1, 'share');
+insert into cab_categories (id, category) values (2, 'regular');
+insert into cab_categories (id, category) values (3, 'green');
+insert into cab_categories (id, category) values (4, 'outstation');
+insert into cab_categories (id, category) values (5, 'rental');
