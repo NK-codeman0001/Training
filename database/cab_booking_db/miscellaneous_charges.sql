50f8ef5c9c64720435176343bc69ba731de9a549
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-02-22 14:52:15 +0530

    add miscellaneous_charges table values

---
+++ database/cab_booking_db/miscellaneous_charges.sql
@@ -0,0 +1,4 @@
+insert into miscellaneous_charges (id, name) values (1,'outstation charge');
+insert into miscellaneous_charges (id, name) values (2, 'waiting  charge');
+insert into miscellaneous_charges (id, name) values (3, 'night charge');
+insert into miscellaneous_charges (id, name) values (4, 'extending charge');
