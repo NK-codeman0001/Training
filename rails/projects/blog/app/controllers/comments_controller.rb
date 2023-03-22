dd346a5eecf3983851baf66fb15d6f7668499da8
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-22 12:11:43 +0530

    Show Comments list of each article in show.html.erb

--- rails/projects/blog/app/controllers/comments_controller.rb
+++ rails/projects/blog/app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
   private
   
   def comment_params
-    params.require(:comment).permit(:comment, :body)
+    params.require(:comment).permit(:commenter, :body)
   end
 
 end
