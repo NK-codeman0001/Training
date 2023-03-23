75ece595d52fff05d2446cf6463341937112d831
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-23 16:16:07 +0530

    create model products and create_products migration

--- rails/projects/blog/db/schema.rb
+++ rails/projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_071550) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_103914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_071550) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
