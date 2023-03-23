4d42e44505e9b97d5a2b8dfa08efd5803cabdf79
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-23 21:44:36 +0530

    Implement migration AddDetailsToProducts

--- rails/projects/blog/db/schema.rb
+++ rails/projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_160131) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_160612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_160131) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "price"
+    t.string "part_number"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
