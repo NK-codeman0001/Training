62b2930d981676ce22c7ffaaf9e52237e2872a75
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-23 21:33:22 +0530

    Implement migration RemovePartNumberFromProducts

--- rails/projects/blog/db/schema.rb
+++ rails/projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_155522) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_160131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,8 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_155522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "price"
-    t.string "part_number"
-    t.index ["part_number"], name: "index_products_on_part_number"
   end
 
   create_table "users", force: :cascade do |t|
