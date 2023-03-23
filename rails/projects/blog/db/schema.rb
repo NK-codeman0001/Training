75e16cdab52c90293d30b7689fddb1861e1d9cde
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-23 22:04:56 +0530

    Implement migration AddUserRefToProducts

--- rails/projects/blog/db/schema.rb
+++ rails/projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_161619) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_163223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_161619) do
     t.datetime "updated_at", null: false
     t.string "part_number"
     t.decimal "price"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_161619) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "products", "users"
 end
