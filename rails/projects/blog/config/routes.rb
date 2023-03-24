a9c38b11a91493be75f3884e851efe525063e92c
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-24 18:58:22 +0530

    Add presence validation in User Model and Modified Routes

--- rails/projects/blog/config/routes.rb
+++ rails/projects/blog/config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "users#new"
+  # root "users#n
   
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 
   resources :users
-  # resources :articles do
-  #   resources :comments
-  # end
+  resources :articles do
+    resources :comments
+  end
 
 end
