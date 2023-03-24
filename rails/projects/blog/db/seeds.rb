7701d1e63541d2019cb4d385ed630e97e857b9d8
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-24 16:44:23 +0530

    Implement seed for adding data to model table (users)

--- rails/projects/blog/db/seeds.rb
+++ rails/projects/blog/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  User.create(name: "Name ##{i}",gender: ["male", "false"].shuffle.first, age: rand(18..30), address: "Address ##{i}", terms_and_conditions: [true,false].shuffle.first)
+end
+
