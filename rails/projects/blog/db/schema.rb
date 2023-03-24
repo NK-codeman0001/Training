62560b219dd820443cb95cf1f5796ac4369f2980
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-24 11:31:18 +0530

    Implement migration AddUsersRefToCustomers

--- rails/projects/blog/db/schema.rb
+++ rails/projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_050622) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_055547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_050622) do
     t.decimal "spend", precision: 10, scale: 2, comment: "in Thousand INR"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
     t.index ["name"], name: "index_customers_on_name"
+    t.index ["users_id"], name: "index_customers_on_users_id"
   end
 
   create_table "products", force: :cascade do |t|
