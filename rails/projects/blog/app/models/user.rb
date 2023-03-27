af1f410ca3deea491da60987e57a91055afb1025
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-27 18:19:11 +0530

    insert comparison validation in age of user model

--- rails/projects/blog/app/models/user.rb
+++ rails/projects/blog/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :terms_and_conditions, acceptance: { message: 'must be abided'}
   validates :email, confirmation: true
   validates :email_confirmation, presence: true
+  validates :age, comparison: {greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
 end
