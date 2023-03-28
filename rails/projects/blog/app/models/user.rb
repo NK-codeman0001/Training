7d873f75640428a881f299002dffde370b0fc4dc
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-28 11:32:23 +0530

    insert numericality validation in age of user model

--- rails/projects/blog/app/models/user.rb
+++ rails/projects/blog/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :terms_and_conditions, acceptance: { message: 'must be abided'}
   validates :email, confirmation: true
   validates :email_confirmation, presence: true
-  validates :age, comparison: {greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
+  validates :age, numericality: {only_integer: true,greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
   validates :country, exclusion: {  in: %w(pakistan russia china), message: "%{value} of %{attribute} for %{model} is reserved."}
   validates :t_size, inclusion: {in: %w(small medium large xl xxl xxxl)}
   validates :password, length: {in: 8..16}
