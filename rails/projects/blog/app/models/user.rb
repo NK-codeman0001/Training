c904b58166e316fd025d3e2c2d421fd30491d75f
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-28 11:06:26 +0530

    insert length validation in password of user model

--- rails/projects/blog/app/models/user.rb
+++ rails/projects/blog/app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :age, comparison: {greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
   validates :country, exclusion: {  in: %w(pakistan russia china), message: "%{value} of %{attribute} for %{model} is reserved."}
   validates :t_size, inclusion: {in: %w(small medium large xl xxl xxxl)}
+  validates :password, length: {in: 8..16}
 end
