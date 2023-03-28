42410bddbb48c9a860299e859b473b07254d9527
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-28 10:38:17 +0530

    insert inclusion validation in t_size of user model

--- rails/projects/blog/app/models/user.rb
+++ rails/projects/blog/app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email_confirmation, presence: true
   validates :age, comparison: {greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
   validates :country, exclusion: {  in: %w(pakistan russia china), message: "%{value} of %{attribute} for %{model} is reserved."}
+  validates :t_size, inclusion: {in: %w(small medium large xl xxl xxxl)}
 end
