c15f8dba8ef0ad91d7285ec9e488863b1d14015c
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-28 11:53:10 +0530

    insert uniqueness validation in email of user model

--- rails/projects/blog/app/models/user.rb
+++ rails/projects/blog/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
   validates :terms_and_conditions, acceptance: { message: 'must be abided'}
-  validates :email, confirmation: true
+  validates :email, confirmation: true, uniqueness: { case_sensitive: false }
   validates :email_confirmation, presence: true
   validates :age, numericality: {only_integer: true,greater_than_or_equal_to: 18, less_than_or_equal_to: 25, other_than: 20}
   validates :country, exclusion: {  in: %w(pakistan russia china), message: "%{value} of %{attribute} for %{model} is reserved."}
