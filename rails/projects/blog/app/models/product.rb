72ba21167708d1740b064875f14d8c8edbfea3a4
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-29 14:51:31 +0530

    implement after_validation callback

--- rails/projects/blog/app/models/product.rb
+++ rails/projects/blog/app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ApplicationRecord
   validates :name, :brand_name, presence: true
-  before_create do
+  
+  # before_create do
+  #   self.name = self.name.downcase
+  #   self.brand_name = self.brand_name.downcase
+  # end
+
+  after_validation :downcase_name_and_brand_name
+
+  def downcase_name_and_brand_name
     self.name = self.name.downcase
     self.brand_name = self.brand_name.downcase
   end
+
+
 end
