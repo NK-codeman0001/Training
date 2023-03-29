b8f71151adcd471a1003d5557b66ee31884e11a0
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-29 14:54:20 +0530

    implement before_validation callback

--- rails/projects/blog/app/models/product.rb
+++ rails/projects/blog/app/models/product.rb
@@ -6,7 +6,15 @@ class Product < ApplicationRecord
   #   self.brand_name = self.brand_name.downcase
   # end
 
-  after_validation :downcase_name_and_brand_name
+  # after_validation :downcase_name_and_brand_name
+
+  # def downcase_name_and_brand_name
+  #   self.name = self.name.downcase
+  #   self.brand_name = self.brand_name.downcase
+  # end
+
+
+  before_validation :downcase_name_and_brand_name
 
   def downcase_name_and_brand_name
     self.name = self.name.downcase
