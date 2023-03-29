2f0a9358b049a6ed805944db1bfe74cd7c940d5d
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-03-29 14:28:35 +0530

    implement before_create do callback

--- rails/projects/blog/app/models/product.rb
+++ rails/projects/blog/app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, :brand_name, presence: true
+  before_create do
+    self.name = self.name.downcase
+    self.brand_name = self.brand_name.downcase
+  end
 end
