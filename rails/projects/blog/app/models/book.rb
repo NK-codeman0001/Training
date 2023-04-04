7b130da595fdcc1798958f5dde259730fb1a6cf5
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-04 19:09:16 +0530

    implement validate option for belongs_to and has_many  method of association

--- rails/projects/blog/app/models/book.rb
+++ rails/projects/blog/app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   # belongs_to :author, foreign_key: true
-  belongs_to :author, touch: true
+  # belongs_to :author, touch: true
+  belongs_to :author
+
+  validates :name, presence: true
+  
 end
