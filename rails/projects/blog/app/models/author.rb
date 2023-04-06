09ea17ea27cb216a5d67c0943411d7efd0705f04
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-06 12:19:46 +0530

    implement Association Callback

--- rails/projects/blog/app/models/author.rb
+++ rails/projects/blog/app/models/author.rb
@@ -7,5 +7,23 @@ end
 class Author < ApplicationRecord
   # has_many :books
   # has_many :books, validate: true
-  has_many :books, extend: BookExtension
+  has_many :books, extend: BookExtension, before_add: :before_add_func
+  # , before_remove: :before_remove_func, after_add: [:after_add_func, :after_add_func_2], after_remove: :after_remove_func
+
+  def before_add_func(book)
+    puts "===before+++Add+++Association+++Callback==="
+    book.published_at = Time.now
+  end
+  def after_add_func(book)
+    puts "===After+++Add+++Association+++Callback==="
+  end
+  def after_add_func_2(book)
+    puts "===After+++Add+++Association+++Callback+++2==="
+  end
+  def before_remove_func(book)
+    puts "===before+++Remove+++Association+++Callback==="
+  end
+  def after_remove_func(book)
+    puts "===After+++Remove+++Association+++Callback==="
+  end
 end
