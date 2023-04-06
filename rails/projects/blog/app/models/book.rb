3068171f9a7eb15cf10d0080c00d7f22db9af7dc
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-06 13:09:03 +0530

    implement scope method of Query Interface

--- rails/projects/blog/app/models/book.rb
+++ rails/projects/blog/app/models/book.rb
@@ -4,6 +4,10 @@ class Book < ApplicationRecord
   belongs_to :author, optional: true
 
   # validates :name, presence: true
+
+  scope :recent_books, -> {where(created_at: (Time.now - 1.day)..Time.now )}
+  scope :recent_books_with_author, -> {recent_books.where.not(author_id: nil)}
+  
   
   
 end
