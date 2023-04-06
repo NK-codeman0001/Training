7ae2fde98b50d978f24a71e2355c45687b1a1da6
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-06 11:29:16 +0530

    implement foreign_key, association_foreign_key, join_table option for has_and_belongs_to_many method of association

--- rails/projects/blog/app/models/assembly.rb
+++ rails/projects/blog/app/models/assembly.rb
@@ -1,3 +1,3 @@
 class Assembly < ApplicationRecord
-  has_and_belongs_to_many :parts
+  has_and_belongs_to_many :parts, foreign_key: 'part_id', association_foreign_key: 'assembly_id', join_table: 'assemblies_parts'
 end
--- rails/projects/blog/app/models/part.rb
+++ rails/projects/blog/app/models/part.rb
@@ -1,3 +1,3 @@
 class Part < ApplicationRecord
- has_and_belongs_to_many :assemblies
+ has_and_belongs_to_many :assemblies, foreign_key: 'part_id', association_foreign_key: 'assembly_id', join_table: 'assemblies_parts'
 end
