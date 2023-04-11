14b0c3f05bd6d410b9c9884d5c24a34c09d5ae54
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-11 14:31:18 +0530

    implement index action of blog-2.0 app

---
+++ rails/projects/blog-2.0/app/controllers/blogs_controller.rb
@@ -0,0 +1,32 @@
+class BlogsController < ApplicationController
+  def index    
+    @blogs = Blog.all.order(created_at: :desc)
+  end
+
+  def new
+  
+  end
+
+  def create
+  
+  end
+
+  def edit
+  
+  end
+
+  def update
+  
+  end
+
+  def destroy
+  
+  end
+
+  private 
+
+  def post_params
+    params.require(:title,:body)
+    
+  end
+end
