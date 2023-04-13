1f5e8ec4d74eb264f0d72408fb0870fca6cf011c
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-13 17:14:59 +0530

    Filter content for normal user and admin

--- rails/projects/blog-2.0/app/controllers/blogs_controller.rb
+++ rails/projects/blog-2.0/app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog, except: [:index, :new, :create]
   def index    
-    @blogs = Blog.all.order(created_at: :desc)
+    @blogs = user_signed_in? ? Blog.all.order(created_at: :desc) : Blog.all.published.order(created_at: :desc)
   end
 
   def show
@@ -54,7 +54,7 @@ class BlogsController < ApplicationController
   private 
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = user_signed_in? ? Blog.find(params[:id]) : Blog.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
