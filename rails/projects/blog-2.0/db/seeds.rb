95b74cdb614592ec01f463207a688ecdd00ecece
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-13 12:04:23 +0530

    add seed to create user(admin) by default

--- rails/projects/blog-2.0/db/seeds.rb
+++ rails/projects/blog-2.0/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+adminuser = User.where(email: "admin.user@bloggits.com").first_or_initialize
+
+adminuser.update(
+  password: "changeit",
+  password_confirmation: "changeit"
+)
