80f84e896606964308ba369d96421afeb986c8d2
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-13 15:01:09 +0530

    Add Scope: draft, published and scheduled

--- rails/projects/blog-2.0/app/models/blog.rb
+++ rails/projects/blog-2.0/app/models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: {maximum: 60}
   validates :body, presence: true, length: {maximum: 255}
   validates :published_at, comparison: {greater_than_or_equal_to: Time.now, message: "can't be in past"}, allow_nil: true
+  
+  scope :draft, -> { where(published_at: nil)}
+  scope :published, -> { where("published_at <= ?", Time.now) }
+  scope :scheduled, -> { where("published_at > ?", Time.now)}
 end
