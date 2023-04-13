7b9db195d5449f5e0fdfabafeeb71383ee4545dc
Neeraj Kumar <neeraj.kumar@berylsystems.com>
2023-04-13 15:11:25 +0530

    add methods: draft?, published?, scheduled?

--- rails/projects/blog-2.0/app/models/blog.rb
+++ rails/projects/blog-2.0/app/models/blog.rb
@@ -6,4 +6,18 @@ class Blog < ApplicationRecord
   scope :draft, -> { where(published_at: nil)}
   scope :published, -> { where("published_at <= ?", Time.now) }
   scope :scheduled, -> { where("published_at > ?", Time.now)}
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.now
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.now
+  end
+
+
 end
